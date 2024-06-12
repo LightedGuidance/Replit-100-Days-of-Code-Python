5a682d5c27f4eb152d9f17e3776b61b127aa045a
LightedGuidance <lightedguidance@proton.me>
2024-06-12 22:01:40 +0100

    Add files via upload

---
+++ Day001.py
@@ -0,0 +1,8 @@
+print("""@LightedGuidance 12/06/2024 13:02
+
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day!""")
+
+print("I am SO EXCITED!")
+
+print("You can follow my progress at replit.com/LightedGuidance")
