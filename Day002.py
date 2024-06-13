2e1ce060153583bf57528a936c16322f4a7baf77
LightedGuidance <lightedguidance@proton.me>
2024-06-13 15:54:50 +0100

    I just built my first interactive code which takes inputs from the user! Day 2 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
    I just built my first interactive code which takes inputs from the user! Day 2 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ Day002.py
@@ -0,0 +1,18 @@
+"""I just built my first interactive code which takes inputs from the user! Day 2 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python"""
+
+print("Definitely not a Phishing Scam")
+print()
+myName = input("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
